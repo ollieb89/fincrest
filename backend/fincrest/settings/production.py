a91bf5f61771c2eafe5954f9c9bc9e62d394f9ae
ollieb89 <buitelaarolivier@gmail.com>
2025-02-10 21:30:57 +0100

    changed deploy script

--- backend/fincrest/settings/production.py
+++ backend/fincrest/settings/production.py
@@ -3,14 +3,14 @@ from fincrest.utils.keyvault import get_secret
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': get_secret("DB-NAME"),
-        'USER': get_secret("DB-USER"),
-        'PASSWORD': get_secret("DB-PASSWORD"),
-        'HOST': get_secret("DB-HOST"),
-        'PORT': "5432",  # Default PostgreSQL port
+        'NAME': get_secret('DB-NAME'),
+        'USER': get_secret('DB-USER'),
+        'PASSWORD': get_secret('DB-PASSWORD'),
+        'HOST': get_secret('DB-HOST'),
+        'PORT': '5432',
     }
 }
 
-SECRET_KEY = get_secret("DJANGO-SECRET-KEY") or "fallback-secret-key"
+SECRET_KEY = get_secret('DJANGO-SECRET-KEY')
 
 ALLOWED_HOSTS = ["fincrest-backend.azurewebsites.net"]
